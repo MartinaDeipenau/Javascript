c2dc48d19c54bd8c97c9ef2d7d9c77aedf3cbc08
martina deipenau <martinadeipenau02@gmail.com>
2022-08-02 15:08:50 -0300

    actualizacion

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
+function verProductos (){
 let welcome = alert("Bienvenido a Brikka Patisserie")
-
 let productos = prompt("Que producto desea adquirir? 1)Lemon pie, 2)Desayuno mini, 3)Baguette de jamon")
 while (productos != 1 && productos != 2 && productos !=3) {
     alert("La opcion elegida es incorrecta")
     productos = prompt("Por favor, ingrese que producto desea adquirir. 1)Lemon pie, 2)Desayuno mini, 3)Baguette de jamon")
 }
-
+}
+verProductos ()
