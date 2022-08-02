52e53f9b90eedf3715420e1cb9cf98437590fb49
martina deipenau <martinadeipenau02@gmail.com>
2022-08-02 13:12:59 -0300

    actualizacion

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
-let year = parseInt(prompt("en que año naciste?:"))
-for(let i= 0;i<10;i++){
-    let curr= 2022 - i
-    console.log("en el año",curr,"tenes",curr-year)
+let ver 
+let nombreProducto 
+
+
+
+function verProducto() {
+    let ver = alert(
+        "Bienvenido a Brikka Patisserie");
+    let nombreProducto = prompt(
+        "Seleccione los productos: Lemon pie, Box desayuno mini, Baguette de jamon");
+    console.log("Producto elegido:", seleccionarPoducto(nombreProducto));
+
+    let producto = prompt("Escriba su producto:");
+    if (producto === "") {
+        alert("No eligio ningun producto");
+    }
+    else {
+        alert("Cargando...");
+    }
+}
+
+function seleccionarProducto(item) {
+    switch (item) {
+        case "Lemon pie":
+            return "Base de galletas de vainilla, con crema de limon y merengue Italiano. Su valor es de $1250";
+        case "Box desayuno mini":
+            return "Contiene: 1 alfajor de maicena y de frutos rojos, 2 medialunas, 1 baguette de jamon mini y 1 jugo exprimido(sabor a eleccion). Su valor es de $1000";
+        case "Baguette de Jamon":
+            return "Pan blanco, jamon y queso a eleccion. Su valor es de $750";
+        default: 
+        return "No eligio producto";
+    }
 }
