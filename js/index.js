e017632e2c3c777712f0fd521e8206e760991fa2
martina deipenau <martinadeipenau02@gmail.com>
2022-08-09 17:45:25 -0300

    array

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
-function verProductos (){
-let welcome = alert("Bienvenido a Brikka Patisserie")
-let productos = prompt("Que producto desea adquirir? 1)Lemon pie, 2)Desayuno mini, 3)Baguette de jamon")
-while (productos != 1 && productos != 2 && productos !=3) {
-    alert("La opcion elegida es incorrecta")
-    productos = prompt("Por favor, ingrese que producto desea adquirir. 1)Lemon pie, 2)Desayuno mini, 3)Baguette de jamon")
+class tarta {
+    constructor(producto, tamaño, precio) {
+        this.producto = producto
+        this.tamaño = tamaño
+        this.precio = precio
+    }
+    price() {
+        //debugger 
+        if (this.producto == "Lemon pie") {
+            let priceL = this.tamaño * mediano
+            return priceL
+        }
+        else if (this.producto == "Cheesecake") {
+            let priceCh = this.tamaño * grande
+            return priceCh
+        }
+        else {
+            let priceO = this.tamaño * chico
+            return priceO
+        }
+    }
 }
+function carrito(){
+    let producto = prompt("Que desea comprar?: Lemon pie, Cheesecake o ambas")
+    producto = producto.toLocaleLowerCase()
+    let tamaño = parseInt(prompt("Ingrese el tamaño de la tarta, (chico,mediano,grande)"))
+    let precio = precio 
+    buy.push (new tarta(producto,tamaño,precio))
+    console.table(buy)
 }
-verProductos ()
+carrito ()
